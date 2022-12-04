a43bc741558255c8b0e1b03481e1ab7784bfd3c8
Dhirodatto Biswas <83452888+vardhiro@users.noreply.github.com>
2022-12-04 12:44:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function vguess()
+{
+	var message = document.getElementById("message");
+
+	var input = document.getElementById('guess').value;
+	var random = Math.floor(Math.random() * 10);
+	if(input == random)
+		message.innerHTML = "<h1>Congratulations</h1>\n<p>You guessed it right</p>";
+	else
+		message.innerHTML = "<h1>Oops</h1>\n<p>You missed it. The number was "+random+"</p>";
+}
